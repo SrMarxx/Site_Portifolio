77eba7a19071b46217a7cd0ca89242618c200644
srmarxx <marcosgabrieldiascardoso66@gmail.com>
2023-12-22 05:32:24 -0300

    Adicionado o verificador de senha no registro

--- index.js
+++ index.js
@@ -19,3 +19,31 @@ btnPopup.addEventListener('click', ()=>{
 iconclose.addEventListener('click', ()=>{
     wrapper.classList.remove('active-popup');
 });
+
+function verifica(senha) {
+    var forca = 0;
+    if (senha.length > 7){                     
+        if (senha.match(/[a-z]+/)) forca++;
+        if (senha.match(/[A-Z]+/)) forca++;
+        if (senha.match(/d+/)) forca++;
+        if (senha.match(/W+/)) forca++;
+    };
+    return forca;
+  }
+  
+  function mostra_res(senha) {
+    var res = verifica(senha);
+    var cor;
+    var msg;
+    if (res < 2) {
+      cor = "red";
+      msg = "Fraca";
+    } else if (res == 2) {
+      cor = "orange";
+      msg = "Média";
+    } else {
+      cor = "green";
+      msg = "Forte";
+    }
+    document.getElementById("forca").innerHTML = "<span style='color:" + cor + ";'>" + msg + "</span>";
+  }
